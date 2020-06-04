be391db34acb27c513219125128daf422dda9370
Nicolas CARRARA <nicarrara@cmin.fr>
2020-06-04 21:49:32 +0200

    Creation ViewController et Navigation TabBar

---
+++ Cotcot'/HatchingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HatchingViewController.swift
+//  Cotcot'
+//
+//  Created by Nicolas CARRARA on 04/06/2020.
+//  Copyright © 2020 Nicolas CARRARA. All rights reserved.
+//
+
+import UIKit
+
+class HatchingViewController: UIViewController {
+
+    @IBOutlet weak var CurrentHatchTable: UITableView!
+    @IBOutlet var noItemsView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CurrentHatchTable.backgroundView=noItemsView
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cotcot'/StatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatsViewController.swift
+//  Cotcot'
+//
+//  Created by Nicolas CARRARA on 04/06/2020.
+//  Copyright © 2020 Nicolas CARRARA. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
