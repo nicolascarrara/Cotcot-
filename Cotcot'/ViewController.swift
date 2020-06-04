141fcfec94ded1ae6eaa586644310f7d3e6d1b38
Nicolas CARRARA <nicarrara@cmin.fr>
2020-06-04 16:26:02 +0200

    Initial Commit

---
+++ Cotcot'/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cotcot'
+//
+//  Created by Nicolas CARRARA on 04/06/2020.
+//  Copyright © 2020 Nicolas CARRARA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
